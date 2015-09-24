daa9b0031d3e3b684c734117e39b4233a0e49fff
JackSparrow1313 <jacksparrow1313@mail.ru>
2015-09-24 23:40:07 +0300

    Create Task5.java

---
+++ git/Task5.java
@@ -0,0 +1,12 @@
+public class Task5 {
+	public static void main (String args[]) {
+		double n = Double.parseDouble (args [0]);
+                double x = Double.parseDouble (args [1]);
+		double A=0; 
+		int i=1;
+		for (i=1; i<=n; i++) {
+			A=Math.cos(x+A);
+		}
+		System.out.println("A="+String.valueOf(A));
+	}
+}
